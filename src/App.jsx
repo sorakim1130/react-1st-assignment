c5a3885c5724867096470d7e7bd0c2d461add76e
sorakim <sora6418@gmail.com>
2024-05-14 06:33:24 +0900

    feat : 리스트 추가, 삭제, 유효성검사 기능개발

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,24 @@ function App() {
   const [users, setUsers] = useState(initialState);
 
   // TODO: 이름과 나이를 각각 상태로 정의하세요. 초기값은 빈문자열("")입니다.
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
 
   const addUser = (e) => {
     e.preventDefault();
     // TODO: 이름과 나이가 모두 입력되지 않았을 때는 alert 처리하고 함수를 종료하세요. 논리합연산자 (||) 를 이용하세요.
+    if (!name || !age) {
+      alert("이름과 나이를 입력해주세요.");
+      return;
+    }
 
     // TODO: 사용자 리스트 상태를 업데이트 하세요. spread operator 를 사용하고, 추가되는 id는 현재 시간을 밀리초 단위로 반환하는 Date.now() 를 사용하세요.
+    setUsers([...users, { id: Date.now(), name, age }]);
   };
 
   const removeUser = (id) => {
     // TODO: filter 메소드를 사용해서 사용자 삭제 로직을 구현해 보세요.
+    setUsers(users.filter((user) => user.id !== id));
   };
 
   return (
@@ -26,13 +34,30 @@ function App() {
       <h1>사용자 리스트</h1>
       <form onSubmit={addUser}>
         {/* TODO: input 태그에 value, onChange 속성을 추가해서 이름과 나이의 상태와 상태변경 로직을 연결하세요 */}
-        <input type="text" placeholder="이름" />
-        <input type="number" placeholder="나이" />
-        <button type="submit">사용자 추가</button>
+        <input
+          type={"text"}
+          placeholder={"이름"}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type={"number"}
+          placeholder={"나이"}
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <button type={"submit"}>사용자 추가</button>
       </form>
       <ul>
         {/* TODO: map 메소드를 이용해서 user 리스트를 렌더링하세요.  */}
         {/* 이름: John, 나이: 20 [삭제] 버튼이 하나의 행에 나올 수 있도록 해보세요. (hint: flex) */}
+        {users.map((user) => (
+          <li className={"list-style"} key={user.id}>
+            <div>이름: {user.name}</div>
+            <div>나이: {user.age}</div>
+            <button onClick={() => removeUser(user.id)}>삭제</button>
+          </li>
+        ))}
       </ul>
     </>
   );
